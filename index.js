ef8c4abb948a388294ceebc28b578ccba37eeb53
RedwikiTV <johanbag290@gmail.com>
2019-07-19 23:00:35 +0900

    아바타 사진을 볼 수 있는 기능 추가

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setGame("/도움말 을 입력하세요!")
+    client.user.setGame("/도움말 을 입력하세요!");
 });
 
 client.on('message', msg => {
+
+    let messageArray = msg.content.split(" ");
+    let args = messageArray.slice[1];
+
     if (msg.content === '/도움말') {
         embed = new Discord.RichEmbed()
             .setColor("#0022ff")
@@ -14,6 +18,9 @@ client.on('message', msg => {
             .addField("깃허브", "[깃허브 org](https://github.com/teamdiscordbotdevelopers)")
             .addField("명령어", "/도움말")
         msg.channel.send(embed);
+    }
+    if (msg.content === '/아바타') {
+        msg.channel.send(msg.author.avatarURL + "\n당신의 아바타 입니다.");
     }
 });
 
