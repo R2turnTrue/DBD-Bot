25b1438911fa93c59ffb4c2db833db9a43c9f6cf
RedwikiTV <johanbag290@gmail.com>
2019-07-19 23:28:29 +0900

    내정보 기능 생성

--- index.js
+++ index.js
@@ -16,11 +16,24 @@ client.on('message', msg => {
             .setColor("#0022ff")
             .setTitle("도움말")
             .addField("깃허브", "[깃허브 org](https://github.com/teamdiscordbotdevelopers)")
-            .addField("명령어", "/도움말\n/아바타")
+            .addField("명령어", "/도움말\n/아바타\n/내정보")
         msg.channel.send(embed);
     }
     if (msg.content === '/아바타') {
         msg.channel.send(msg.author.avatarURL + "\n당신의 아바타 입니다.");
+    }
+
+    if (msg.content === '/내정보') {
+        embed = new Discord.RichEmbed()
+            .setTitle("내 정보")
+            .setColor("#ce6bdb")
+            .addField("사용자 이름", msg.author.username)
+            .addField("닉네임", msg.member.nickname)
+            .addField("전체 사용자 이름", msg.author.tag)
+            .addField("계정이 만들어진 날", msg.author.createdAt)
+            .addField("이 유저가 이 서버에 온 날", msg.member.joinedAt)
+            .setThumbnail(msg.author.avatarURL)
+        msg.channel.send(embed)
     }
 });
 
